aa94e14e34049c8d8c7cd4277b60aa4692697415
Nguyen-Tuan-Canh <ntcanha23045@cusc.ctu.edu.vn>
2024-11-19 08:21:57 +0700

    second commit

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/users');
+        await mongoose.connect('mongodb+srv://A23045:A23045@cluster0.9sg9q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Kết nối MongoDB thành công');
     } catch (err) {
         console.error('Lỗi kết nối MongoDB:', err);
